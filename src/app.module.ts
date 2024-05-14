8f1154aff76528ccff6ee53c1624a3c9f532fe75
1tang2bang92 <1tang2bang92@gmail.com>
2024-05-14 22:08:09 +0900

    권한기능 제작완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService, PrismaService } from './app.service'
+import { AuthModule } from './auth/auth.module'
 
+@Global()
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
